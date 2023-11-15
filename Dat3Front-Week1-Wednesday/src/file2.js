9f7525fbf9f0ad4067df8bfed2d093c582b385ba
MagKon <magnus.kongstad@gmail.com>
2023-11-15 13:02:52 +0100

    Exc 1 done

---
+++ Dat3Front-Week1-Wednesday/src/file2.js
@@ -0,0 +1,10 @@
+const obj = {
+    firstName: "Kurt",
+    lastName: "Wonnegut",
+    gender : "Male",
+    email: "kurt@wonnegut.dk"
+  };
+export default obj;
+
+export const males = ["Peter","Jan", "Kurt"];
+export const females = ["Helle", "Janne","Sarah", "Tina"];
